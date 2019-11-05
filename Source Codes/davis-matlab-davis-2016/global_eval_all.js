8ece72e3a27df2dfc92cb1bba2b3385ec92397ef
zhxl0903 <zhxl0903@gmail.com>
2019-11-05 11:26:36 -0500

    Changed evaluation dataset to 'all'. DAVIS Bnechmark is now very close to paper result for LDOF.

---
+++ Source Codes/davis-matlab-davis-2016/global_eval_all.js
@@ -0,0 +1,15 @@
+{
+	"Tgt": "0.095",
+	"techniques": [
+		"LDOF_MP_Net_CRF"
+	],
+	"LDOF_MP_Net_CRF": {
+		"Jmean": "0.699",
+		"Jrecall": "0.832",
+		"Jdecay": "0.052",
+		"Fmean": "0.665",
+		"Frecall": "0.785",
+		"Fdecay": "0.064",
+		"T": "0.684"
+	}
+}
--- Source Codes/davis-matlab-davis-2016/global_eval_val.js
+++ Source Codes/davis-matlab-davis-2016/global_eval_val.js
@@ -6,10 +6,10 @@
 	"LDOF_MP_Net_CRF": {
 		"Jmean": "0.703",
 		"Jrecall": "0.849",
-		"Jdecay": "0.019",
+		"Jdecay": "0.018",
 		"Fmean": "0.661",
 		"Frecall": "0.790",
 		"Fdecay": "0.029",
-		"T": "0.567"
+		"T": "0.568"
 	}
 }
